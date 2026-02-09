3d45c7b37bd1e06ae9b346817f6e026b92e03a13
CreaSoley <artcreatifmeg@gmail.com>
2026-02-09 10:26:48 +0100

    Create techniquesDeBase.js

---
+++ techniquesDeBase.js
@@ -0,0 +1,35 @@
+const techniquesDeBase = [
+  { technique: 1, type: "Atemi", nom: "Uraken uchi" },
+  { technique: 1, type: "Clé", nom: "Kote gaeshi" },
+  { technique: 1, type: "Projection", nom: "O soto gari" },
+
+  { technique: 2, type: "Atemi", nom: "Mae hiza geri" },
+  { technique: 2, type: "Clé", nom: "Waki gatame" },
+  { technique: 2, type: "Projection", nom: "Taï otoshi" },
+
+  { technique: 3, type: "Atemi", nom: "Yoko empi" },
+  { technique: 3, type: "Clé", nom: "Juji ude gatame" },
+  { technique: 3, type: "Projection", nom: "Ippon seoie nage" },
+
+  { technique: 4, type: "Atemi", nom: "Chudan mae geri" },
+  { technique: 4, type: "Clé", nom: "Yuki shigae" },
+  { technique: 4, type: "Projection", nom: "Waki otoshi" },
+
+  { technique: 5, type: "Atemi", nom: "Jodan shuto uchi" },
+  { technique: 5, type: "Clé", nom: "Hiji dori ura" },
+  { technique: 5, type: "Projection", nom: "Uki waza" },
+
+  { technique: 6, type: "Atemi", nom: "Chudan Mawashi geri" },
+  { technique: 6, type: "Clé", nom: "Shiho nage" },
+  { technique: 6, type: "Projection", nom: "Haraï goshi" },
+
+  { technique: 7, type: "Atemi", nom: "Chudan kizami tsuki" },
+  { technique: 7, type: "Clé", nom: "Tembin" },
+  { technique: 7, type: "Projection", nom: "Ushiro goshi" },
+
+  { technique: 8, type: "Atemi", nom: "Yoko fumikomi" },
+  { technique: 8, type: "Clé", nom: "Hashi mawashi" },
+  { technique: 8, type: "Projection", nom: "Kata Hizaguruma" }
+];
+
+export default techniquesDeBase;
